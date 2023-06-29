aa044599ac2a56c32304ed4217c63dd02fbb5765
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2023-06-30 00:05:12 +0600

    add authentication and authorization middleware

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +35,10 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({storage});
+
+/*Routes*/
+app.post("/auth/register", upload.single("picture"), register);
+app.use("/auth", authRoutes);
 
 /*Mongoose setup*/
 const PORT = process.env.PORT || 3002;
---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        let token = req.header("Authorization");
+
+        if(!token){
+            return res.status(403).send("Access Denied");
+        }
+
+        if(token.startsWith("Bearer ")){
+            token =  token.slice(7, token.length);
+        }
+
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
+        next();
+    } catch (error) {
+        res.status(500).json({ error: error.message});
+    }
+}
